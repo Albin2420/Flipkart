2060a0f29a3865cbe7877fe4a7f5cb1dc3c7d4fd
Albin Antony <albinantony2420@gmail.com>
2026-02-19 12:33:08 +0530

    make ui more efficently added the homestate

--- lib/src/presentation/controller/home_controller/home_controller.dart
+++ lib/src/presentation/controller/home_controller/home_controller.dart
@@ -1,14 +1,16 @@
 import 'dart:developer';
+import 'package:flipkart/src/core/data_state/data_state.dart';
 import 'package:flipkart/src/data/models/product_model/product_model.dart';
 import 'package:flipkart/src/data/repositories/product_remote_repository_impl/product_remote_repository_impl.dart';
 import 'package:flipkart/src/domain/repositories/product_remote_repository.dart/product_remote_repository.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
+  var state = DataState.initial.obs;
   final ProductRepository ptr = ProductRemoteRepositoryImpl();
 
   RxList<Product> productsList = <Product>[].obs;
-  RxBool isLoading = false.obs;
+
   RxString error = ''.obs;
 
   @override
@@ -19,7 +21,7 @@ class HomeController extends GetxController {
 
   Future<void> fetchproduct() async {
     try {
-      isLoading.value = true;
+      state.value = DataState.loading;
       error.value = '';
 
       final res = await ptr.fetchproducts();
@@ -27,16 +29,17 @@ class HomeController extends GetxController {
       res.fold(
         (l) {
           error.value = "Failed to load products";
+          state.value = DataState.error;
           log("API ERROR: $l");
         },
         (R) {
+          state.value = DataState.success;
           productsList.value = R["products"];
         },
       );
     } catch (e) {
       error.value = e.toString();
-    } finally {
-      isLoading.value = false;
+      state.value = DataState.error;
     }
   }
 }
